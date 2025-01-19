7951fd60161f345583662a16189d015272b8dfc0
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-18 22:38:23 -0600

    selected projct styling

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -4,16 +4,16 @@ import Button from "./Button";
 const SelectedProject = ({ projects }) => {
   const { title, description, dueDate } = projects;
 
-  const formatteddate = new Date(dueDate).toLocaleDateString('en-US');
+  const formatteddate = new Date(dueDate).toLocaleDateString("en-US");
   return (
-    <div>
-      <header>
-        <div>
-          <h1>{title}</h1>
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-2 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">{title}</h1>
           <Button>Delete</Button>
         </div>
-        <p>{formatteddate}</p>
-        <p>{description}</p>
+        <p className="mb-4 text-stone-400">{formatteddate}</p>
+        <p className="text-stone-400 whitespace-pre-wrap">{description}</p>
       </header>
     </div>
   );
