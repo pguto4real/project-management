1f54217ed0e9af2c7478629c1bd57fae78dda122
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-18 20:59:00 -0600

    p and label sty;ing

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Input = ({ label, isTextarea, ...props }) => {
   return (
-    <p>
-      <label htmlFor="">{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
       {isTextarea ? <textarea {...props} /> : <input {...props} />}
     </p>
   );
