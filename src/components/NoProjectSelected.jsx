13e66513708705bdc54b5756aa6bf5fedf99b6db
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-18 21:05:56 -0600

    No project selected

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NoProjectSelected = () => {
+  return (
+    <div>
+      <img />
+      <h2>No Project Selected</h2>
+      <p>Select a project or get started with a new one</p>
+      <p>
+        <button>Create new project</button>
+      </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
