78a3218a5d119535e435699efbca5b477b5a584b
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-19 17:57:09 -0600

    NewTask

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Button from "./Button";
 import Tasks from "./Tasks";
 
-const SelectedProject = ({ project,onDelete }) => {
-    console.log(project)
-  const { title, description, dueDate,id } = project;
+const SelectedProject = ({ project, onDelete }) => {
+  console.log(project);
+  const { title, description, dueDate, id } = project;
 
   const formatteddate = new Date(dueDate).toLocaleDateString("en-US");
   return (
@@ -12,12 +12,12 @@ const SelectedProject = ({ project,onDelete }) => {
       <header className="pb-4 mb-4 border-b-2 border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{title}</h1>
-          <Button onClick={()=>onDelete(id)}>Delete</Button>
+          <Button onClick={() => onDelete(id)}>Delete</Button>
         </div>
         <p className="mb-4 text-stone-400">{formatteddate}</p>
         <p className="text-stone-400 whitespace-pre-wrap">{description}</p>
       </header>
-      <Tasks/>
+      <Tasks />
     </div>
   );
 };
