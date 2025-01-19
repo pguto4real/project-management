ae26dee0d52cd7bdab07031cba698d0e5fe1bd41
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-18 21:13:16 -0600

    style no project side bar

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import noProjectImage from '../assets/no-projects.png'
 
 const NoProjectSelected = () => {
   return (
-    <div>
-      <img />
-      <h2>No Project Selected</h2>
-      <p>Select a project or get started with a new one</p>
-      <p>
-        <button>Create new project</button>
+    <div className="mt-24 text-center w-2/3">
+      <img src={noProjectImage} className="w-16 h-16 object-contain mx-auto"/>
+      <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
+      <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
+      <p className="mt-8">
+        <button className="px-4 py-2 text-xs md:text-base rounded-md 
+        bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Create new project</button>
       </p>
     </div>
   );
